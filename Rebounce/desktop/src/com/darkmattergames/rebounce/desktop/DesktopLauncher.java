52f8cc400b73c0c59b9e9b286a94da6fdd4aac78
sjanjua <sjanjua94@gmail.com>
2016-06-14 01:07:21 -0400

    Added Project Files

---
+++ Rebounce/desktop/src/com/darkmattergames/rebounce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.darkmattergames.rebounce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.darkmattergames.rebounce.Rebounce;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rebounce(), config);
+	}
+}
